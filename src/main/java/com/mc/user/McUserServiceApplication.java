ae75223a83f8c644701202da8a1814e1a52d2c41
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-06-22 13:38:48 +0000

    First commit.

---
+++ src/main/java/com/mc/user/McUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mc.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mc/user/McUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
